f8f645af0717ca1c5d37ce3117c7fc07115211ee
heyzqt <heyzqt@qq.com>
2017-11-30 15:01:08 +0800

    设置图片的缩放

--- app/src/main/java/com/heyzqt/googletraingdemo3/MainActivity.java
+++ app/src/main/java/com/heyzqt/googletraingdemo3/MainActivity.java
@@ -3,6 +3,8 @@ package com.heyzqt.googletraingdemo3;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements AudioManager.OnAu
     private static final int INTENT_IMAGE_PICTURE = 101;
 
     private Button mFullPicBtn;
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +58,50 @@ public class MainActivity extends AppCompatActivity implements AudioManager.OnAu
             }
         });
 
+        mImageView = (ImageView) findViewById(R.id.imageview1);
+        setPic();
+
         //use Camera to take a picture
         //takePicture();
 
-        galleryAddPic();
+        //galleryAddPic();
     }
 
+    private void setPic() {
+        // get picture width and height
+//        int imgWidth = mImageView.getWidth();
+//        int imgHeight = mImageView.getHeight();
+
+        // get bitmap width and height
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/imgg.jpg";
+        File f = new File(path);
+        if (f.exists()) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(path, options);
+            int bitmapWidth = options.outWidth;
+            int bitmapHeight = options.outHeight;
+
+            //find a flaxible scale size
+            //int scalesize = Math.min(bitmapWidth / imgWidth, bitmapHeight / imgHeight);
+
+            //set options
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = 4;
+            options.inPurgeable = true;
+
+            //set image view
+            Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+            mImageView.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    //add the pictures to the gallery
     public void galleryAddPic() throws NullPointerException {
         //Uri uri = Uri.parse("file:///storage/emulated/0/Pictures/JPEG_171130_102123_-234018204.jpg");
         Intent scanintent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
